b3fe2a36e6fdc655c55c3240d40c33bc1dbe50a9
soojeonglee <eee.soojeong@gmail.com>
2021-11-29 22:16:11 +0900

    next.js tutorial

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import '../styles/global.css'
+
+export default function App({ Component, pageProps}) {
+  return (
+   <Component {...pageProps} />
+  )
+}
